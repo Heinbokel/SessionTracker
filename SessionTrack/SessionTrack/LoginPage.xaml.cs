fbd6feb743fc4e31cc237f4405c2ca3f8bf45d60
Robert Heinbokel <RobertHeinbokel@gmail.com>
2018-05-05 15:28:11 -0400

    Login Page Done

---
+++ SessionTrack/SessionTrack/LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SessionTrack
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_RegisterButtonClicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
